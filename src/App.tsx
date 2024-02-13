e6373adde17ff7e7c7e43eb922bf41bb10617b80
Matheus Cristofoli <matfrietasc@gmail.com>
2024-02-12 22:41:18 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import Field from '@assets/Field.svg'
+
+export default function App() {
+	return (
+		<div className='bg-red-800 h-screen flex flex-row'>
+			<div className='bg-green-900 w-1/4 relative flex flex-col'>
+				<h2 className='text-3xl p-2 w-full bg-slate-100 text-center'>Team A</h2>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-red-300 h-1/5'></div>
+				<div className='bg-blue-300 h-1/5'></div>
+				<div className='bg-gray-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+			</div>
+			<div className='bg-[#008000] w-full flex items-center justify-center relative z-0'>
+				<h1 className='absolute top-4 mx-auto z-10 text-3xl text-white'>
+					Puppy Bowl
+				</h1>
+				<object type='image/svg+xml' data={Field}>
+					Field SVG Animation
+				</object>
+			</div>
+			<div className='bg-amber-900 w-1/4 relative flex flex-col'>
+				<h2 className='text-3xl p-2 w-full bg-slate-100 text-center'>Team B</h2>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+				<div className='bg-yellow-300 h-1/5'></div>
+			</div>
+		</div>
+	)
+}
