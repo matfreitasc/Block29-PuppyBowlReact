1fc2ce78229eb3cfc54112eee7b59893b11a35e6
Matheus Cristofoli <matfrietasc@gmail.com>
2024-02-15 23:51:57 -0500

    Added filter

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,11 +2,12 @@ import Modal from '@components/Modal.tsx'
 import { useState } from 'react'
 import { useLoaderData, useSearchParams } from 'react-router-dom'
 import { Player } from 'types/types.ts'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const App = () => {
   const players = useLoaderData() as Player[]
   const [searchParams, setSearchParams] = useSearchParams()
+  const [selectedPlayer, setSelectedPlayer] = useState<Player | null>(null)
 
   const [open, setOpen] = useState(false)
   const onClose = () => setOpen(false)
@@ -26,12 +27,6 @@ const App = () => {
             }}
             className='h-12 p-2 text-2xl text-black bg-gray-300 text-2 w-96 rounded-xl'
           />
-          <button
-            type='submit'
-            className='absolute right-0 top-[50%] h-full translate-y-[-50%] rounded-br-xl rounded-tr-xl border-2 border-gray-600 bg-gray-600 px-4 font-sans'
-          >
-            Search
-          </button>
         </div>
 
         <button
@@ -44,25 +39,66 @@ const App = () => {
           New Player
         </button>
       </nav>
-      <motion.div className='flex flex-wrap justify-center py-10 gap-x-8 gap-y-4'>
-        {players
-          .filter((player) => player.name.toLowerCase().includes(searchParams.get('search') || ''))
-          .map((player) => (
-            // we will display the players here
-            <div
-              key={player.id}
-              className='flex flex-col flex-wrap items-center justify-center gap-4 p-2 bg-gray-200 rounded-lg cursor-pointer w-72 h-80'
-              onClick={() => {
-                setOpen(true)
-              }}
-            >
-              <img src={player.imageUrl} alt={player.name} className='object-cover w-32 rounded-lg aspect-square' />
-              <h1>Name: {player.name}</h1>
-              <h1>Breed: {player.breed}</h1>
-              <span className='px-10 py-1 text-white rounded-md bg-slate-700'> Click for more info</span>
-            </div>
-          ))}
-      </motion.div>
+      <AnimatePresence>
+        {selectedPlayer ? (
+          <div>
+            {players.map((player) => {
+              if (player.id === selectedPlayer.id) {
+                return (
+                  <motion.div
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0 }}
+                    transition={{ duration: 1, type: 'spring', staggerChildren: 1 }}
+                    key={player.id}
+                    className='flex flex-col flex-wrap items-center justify-center gap-4 p-2 bg-gray-200 rounded-lg cursor-pointer w-72 h-80'
+                    onClick={() => {
+                      setSelectedPlayer(player)
+                    }}
+                  >
+                    <img
+                      src={player.imageUrl}
+                      alt={player.name}
+                      className='object-cover w-32 rounded-lg aspect-square'
+                    />
+                    <h1>Name: {player.name}</h1>
+                    <h1>Breed: {player.breed}</h1>
+                    <span className='px-10 py-1 text-white rounded-md bg-slate-700'> Click for more info</span>
+                  </motion.div>
+                )
+              }
+            })}
+          </div>
+        ) : (
+          <motion.div className='flex flex-wrap justify-center py-10 gap-x-8 gap-y-4'>
+            {players
+              .filter((player) =>
+                searchParams.get('search')
+                  ? player.name.toLowerCase().includes(searchParams.get('search') as string)
+                  : player
+              )
+              .map((player) => (
+                // we will display the players here
+                <motion.div
+                  initial={{ opacity: 0, scale: 0 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0 }}
+                  transition={{ duration: 1, type: 'spring', staggerChildren: 1 }}
+                  key={player.id}
+                  className='flex flex-col flex-wrap items-center justify-center gap-4 p-2 bg-gray-200 rounded-lg cursor-pointer w-72 h-80'
+                  onClick={() => {
+                    setSelectedPlayer(player)
+                  }}
+                >
+                  <img src={player.imageUrl} alt={player.name} className='object-cover w-32 rounded-lg aspect-square' />
+                  <h1>Name: {player.name}</h1>
+                  <h1>Breed: {player.breed}</h1>
+                  <span className='px-10 py-1 text-white rounded-md bg-slate-700'> Click for more info</span>
+                </motion.div>
+              ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
       <Modal open={open} onClose={onClose} />
     </div>
   )
